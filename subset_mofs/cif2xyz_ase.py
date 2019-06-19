5a7722ee504bfbbf6ed2ad84874018cffbae75da
Aditi Krishnaprayan <aditikrishnaprayan@MBP13-AKrishnaprayan.local>
2019-06-18 18:17:01 -0700

    cif2xyz conversion

--- subset_mofs/cif2xyz_ase.py
+++ subset_mofs/cif2xyz_ase.py
@@ -1,11 +1,21 @@
 import ase
 import ase.io
+import os
+from typing import List
 
-zeoliteFiles = "subset_mof_list.txt"
+def cif2xyz(structure_list : List[str]) -> None:
+    """
+    Converts cif files to xyz files
+    Arg: name of file containing all the cif structures
+    """
+    if os.path.exists('./xyz_structures') == False:
+        os.mkdir('xyz_structures')
+    with open(structure_list, "r") as f:
+        for line in f:
+            line = line.strip()
+            stripped_line = line[:-4] #remove ".cif" from each line, for labeling
+            atoms = ase.io.read(line) #read in cif line
+            ase.io.write('./xyz_structures/' + stripped_line + '.xyz', atoms)
 
-with open(zeoliteFiles, "r") as f:
-    for line in f:
-        line = line.strip()
-        stripped_line = line[:-4] #remove ".cif" from each line, for labeling
-        atoms = ase.io.read(line) #read in cif line
-        ase.io.write(stripped_line + '.xyz', atoms)
+if __name__ == '__main__':
+    cif2xyz('subset_mof_list.txt')
