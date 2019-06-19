dceb49938133c3a2d66d9804cbd8a960a268139c
Aditi Krishnaprayan <aditikrishnaprayan@MBP13-AKrishnaprayan.local>
2019-06-18 18:46:22 -0700

    end to end testing

--- subset_mofs/cif2xyz_ase.py
+++ subset_mofs/cif2xyz_ase.py
@@ -3,26 +3,21 @@ import ase
 import ase.io
 import os
 from typing import List
+from subset_mofs import MOF_TDA_PATH
 
-<<<<<<< HEAD
-def cif2xyz(structure_list : List[str]) -> None:
+def cif2xyz(structure_list : str) -> None:
     """
     Converts cif files to xyz files
     Arg: name of file containing all the cif structures
     """
-    if os.path.exists('./xyz_structures') == False:
+    if os.path.exists(os.path.join(MOF_TDA_PATH, './xyz_structures')) == False:
         os.mkdir('xyz_structures')
     with open(structure_list, "r") as f:
         for line in f:
             line = line.strip()
             stripped_line = line[:-4] #remove ".cif" from each line, for labeling
             atoms = ase.io.read(line) #read in cif line
-            ase.io.write('./xyz_structures/' + stripped_line + '.xyz', atoms)
-=======
-from subset_mofs import MOF_TDA_PATH
-
-zeoliteFiles = os.path.join(MOF_TDA_PATH, "subset_mof_list.txt")
->>>>>>> 56633fd3123deac63daa32970def503d1c7bb69a
+            ase.io.write(os.path.join(MOF_TDA_PATH, './xyz_structures/' + stripped_line + '.xyz'), atoms)
 
 if __name__ == '__main__':
     cif2xyz('subset_mof_list.txt')
---
+++ subset_mofs/tests/test_end2end.py
@@ -0,0 +1,43 @@
+import unittest
+import os
+import numpy as np
+import pickle
+
+from subset_mofs import MOF_TDA_PATH
+from subset_mofs.narrow_mof_dataset import get_lowest_volumes
+from subset_mofs.cif2xyz_ase import cif2xyz
+from subset_mofs.create_cubic_cells import copies_to_fill_cell, lattice_param
+from subset_mofs.get_delaunay_triangulation import get_delaunay_simplices, get_persistence
+from pymatgen import Structure, Lattice
+from monty.tempfile import ScratchDir
+from pymatgen.io.ase import AseAtomsAdaptor
+from ase.io.xyz import write_xyz
+
+class EndToEndTest(unittest.TestCase):
+    def workflow(self):
+        filepath = []
+        with open(os.path.join(MOF_TDA_PATH, 'allMOFs_without_disorder.txt'),'r') as f:
+            for line in f:
+                line = line.strip()
+                filepath.append(line)
+        total_volume = pickle.load(open('tot_volume.pkl', 'rb'))
+        lowest_mof_list = get_lowest_volumes(1, total_volume, filepath)
+        #also generates a file that can be read by ase
+        cif2xyz('mof_structures.txt')
+
+        filepaths = []
+        MOF_FILES = os.path.join(MOF_TDA_PATH, 'mof_structures.txt')
+        with open(MOF_FILES,'r') as f:
+            for line in f:
+                line = line.strip()
+                stripped_line = line[:-4] #strip .cif off
+                filepaths.append(stripped_line + ".xyz")
+
+        lattice_csts = lattice_param(filepaths[0])
+        new_cell = copies_to_fill_cell(90, filepaths[0], lattice_csts)
+        simplices = get_delaunay_simplices(new_cell)
+        dgms = get_persistence(simplices)
+        print(dgms)
+
+if __name__ == '__main__':
+    unittest.main()
