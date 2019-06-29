d083656260cde054e489ef52d2f11e0fe2026327
Joseph Montoya <joseph.montoya@tri.global>
2019-06-28 17:20:05 -0700

    create initial mofdb doc

---
+++ mof_tda/ingest/mofdb.py
@@ -0,0 +1,7 @@
+"""
+This module contains functionality for the ingestion of data from the MOF DB
+hosted by the Snurr research lab at Northwestern University
+
+The MOF DB frontend is hosted at https://mof.tech.northwestern.edu
+"""
+import requests
