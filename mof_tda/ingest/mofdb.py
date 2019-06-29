36e4cf2c5232c1ffa64dcd0763fc03ee5f60892f
Joseph Montoya <joseph.montoya@tri.global>
2019-06-28 19:13:09 -0700

    working revision

--- mof_tda/ingest/mofdb.py
+++ mof_tda/ingest/mofdb.py
@@ -1,7 +1,87 @@
 """
 This module contains functionality for the ingestion of data from the MOF DB
-hosted by the Snurr research lab at Northwestern University
+hosted by the Snurr research lab at Northwestern University.  Note that
+none of this code is based on any explicit documentation of the MOF DB,
+but on inferences from inspection of the frontend code and query parameters.
+
+As such, future iterations may need refactoring if the API changes.
 
 The MOF DB frontend is hosted at https://mof.tech.northwestern.edu
 """
 import requests
+import json
+from tqdm import tqdm
+from multiprocessing import Pool
+from monty.serialization import dumpfn, loadfn
+from mof_tda import MOF_TDA_PATH
+import os
+
+
+MOFDB_URL = "https://mof.tech.northwestern.edu"
+
+def fetch_mofdb_isotherm(iso_id):
+    """
+    Fetches a json document from the MOFDB API for an isotherm
+    for a given structure
+
+    Args:
+        iso_id (int): id for query parameter in URL
+
+    Returns:
+        (dict) document corresponding to fetched json from server
+
+    """
+    response = requests.get("{}/iso.php?id={}".format(MOFDB_URL, iso_id))
+    return json.loads(response.content)
+
+
+def fetch_many_docs(iso_ids, nproc=1):
+    """
+
+    Args:
+        iso_ids ([int]): list of iso_ids to fetch
+        nproc (int): number of processes to use, if greater than 1, will use
+            larger number of processes
+
+    Returns:
+        ([dict]) list of fetched documents
+
+    """
+    if nproc > 1:
+        with Pool(nproc) as pool:
+            return list(tqdm(pool.imap(fetch_mofdb_isotherm, iso_ids),
+                             total=len(iso_ids)))
+        pass
+    else:
+        return [fetch_mofdb_isotherm(iso_id) for iso_id in tqdm(iso_ids)]
+
+
+def fetch_preset(output_filename='mofdb_isotherms.json', nproc=8):
+    """
+    Fetches preset list of docs determined via trial and error,
+
+    An initial query via the frontend on 06/28/2019 showed 12870,
+    and subsequent sampling of ids from 8000-25000 yielded all 12820.
+    Successful query ids were stored in indices.json, up which this
+    function should be able extract all of the relevant data.
+
+    Args:
+        output_filename (str): output filename for all collected docs
+        nproc (int): number of processes to use
+
+    Returns:
+        (List): list of isotherm documents
+    """
+    # Load indices from json doc
+    iso_ids = loadfn(os.path.join(MOF_TDA_PATH, "ingest", "indices.json"))
+
+    # Fetch all docs from ids
+    isotherms = fetch_many_docs(iso_ids, nproc=nproc)
+
+    # Dump to json
+    dumpfn(isotherms, "mofdb_isotherms.json")
+    return isotherms
+
+
+if __name__ == "__main__":
+    fetch_preset()
--- mof_tda/ingest/tests/test_mofdb.py
+++ mof_tda/ingest/tests/test_mofdb.py
@@ -1,15 +1,22 @@
+# Copyright 2019 (c), Toyota Research Institute, All rights reserved
 import unittest
+from mof_tda.ingest.mofdb import fetch_mofdb_isotherm, fetch_many_docs
 
 
 class MofDbIngestionTest(unittest.TestCase):
     def test_json_fetch(self):
         # Test fetching of legit data
-        isotherm = fetch_mofdb_isotherm_data(id=23076)
+        isotherm = fetch_mofdb_isotherm(iso_id=23076)
         self.assertEqual(isotherm['temperature'], 77)
 
         # Test fetching of bad data
-        isotherm = fetch_mofdb_isotherm_data(id=23077)
+        isotherm = fetch_mofdb_isotherm(iso_id=23077)
         self.assertEqual(isotherm['temperature'], None)
+
+        # Test multifetching
+        isotherms = fetch_many_docs(iso_ids=[23076, 23077])
+        self.assertEqual(isotherms[0]['temperature'], 77)
+        self.assertEqual(isotherms[1]['temperature'], None)
 
 
 if __name__ == '__main__':
