aa23b82e16c81b3266cd2d539c36e06e1c9bc795
Joseph Montoya <joseph.montoya@tri.global>
2019-08-07 10:40:27 -0700

    add xyz_structures and fix main test

--- mof_tda/tests/test_main.py
+++ mof_tda/tests/test_main.py
@@ -7,12 +7,13 @@ from mof_tda.main import main
 
 class EndToEndTest(unittest.TestCase):
     def test_main(self):
-        persistence_diagram, current_file = main(0, 1)
+        persistence_diagram = main("CICYIX_clean.xyz")
 
+        # TODO: don't think this is necessary any more
         # Check that the pickled file is the same as the item in the list: mof_structures contains cif
-        with open(os.path.join(MOF_TDA_PATH, "mof_structures.txt"), "r") as f:
-            data = f.readlines()
-            self.assertEqual(current_file[:-4], data[0][:-5])
+        # with open(os.path.join(MOF_TDA_PATH, "mof_structures.txt"), "r") as f:
+        #     data = f.readlines()
+        #     self.assertEqual(current_file[:-4], data[0][:-5])
 
 
 if __name__ == '__main__':
