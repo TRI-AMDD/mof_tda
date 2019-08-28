d3d125259549019662afcfc3a64a9305d8b872e9
Joseph Montoya <joseph.montoya@tri.global>
2019-08-28 14:38:42 -0700

    update tests

--- mof_tda/ingest/tests/test_builder.py
+++ mof_tda/ingest/tests/test_builder.py
@@ -5,9 +5,11 @@ import os
 # Set test environ
 os.environ["MOF_TDA_DB_MODE"] = "test"
 
-from mof_tda import MOF_TDA_PATH
+from mof_tda import MOF_TDA_PATH, MOF_TDA_TEST_FILE_PATH
 from mof_tda.ingest.builder import MofDbStructureBuilder
 from mof_tda.ingest.docdb import get_db
+
+from maggma.runner import Runner
 
 
 class BuilderTest(unittest.TestCase):
@@ -23,8 +25,10 @@ class BuilderTest(unittest.TestCase):
         self.test_db.wasserstein_distances.drop()
 
     def test_structure_builder(self):
-
-
+        builder = MofDbStructureBuilder(source=MOF_TDA_TEST_FILE_PATH,
+                                        target=self.test_db.structures)
+        runner = Runner([builder])
+        runner.run()
 
 
 if __name__ == '__main__':
